404451524c2d4185d08ddb5a7909832156f52ac0
aastha-sriv18 <aastha18p@gmail.com>
2026-06-26 10:04:01 +0530

    Exercise 5

---
+++ Exercise5/cohortstracker/src/CohortDetails.js
@@ -0,0 +1,35 @@
+import styles from './CohortDetails.module.css';
+
+function CohortDetails(props) {
+
+    return (
+        <div className={styles.box}>
+            <h3
+                style={{
+                    color:
+                        props.currentStatus === "Ongoing"
+                            ? "green"
+                            : "blue"
+                }}
+            >
+                {props.name}
+            </h3>
+
+            <dl>
+                <dt>Started On</dt>
+                <dd>{props.startDate}</dd>
+
+                <dt>Current Status</dt>
+                <dd>{props.currentStatus}</dd>
+
+                <dt>Coach</dt>
+                <dd>{props.coach}</dd>
+
+                <dt>Trainer</dt>
+                <dd>{props.trainer}</dd>
+            </dl>
+        </div>
+    );
+}
+
+export default CohortDetails;
