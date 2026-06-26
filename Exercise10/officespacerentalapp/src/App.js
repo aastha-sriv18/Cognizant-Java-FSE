8d28a8fb3fff4408ce4cee044f4ac7d56c9788ea
aastha-sriv18 <aastha18p@gmail.com>
2026-06-26 11:26:47 +0530

    Exercise 10

---
+++ Exercise10/officespacerentalapp/src/App.js
@@ -0,0 +1,71 @@
+import './App.css';
+import officeImg from './office.png';
+
+function App() {
+
+  const offices = [
+    {
+      Name: "DBS",
+      Rent: 50000,
+      Address: "Chennai",
+      Image: officeImg
+    },
+    {
+      Name: "Regus",
+      Rent: 70000,
+      Address: "Bangalore",
+      Image: officeImg
+    },
+    {
+      Name: "SmartWorks",
+      Rent: 55000,
+      Address: "Hyderabad",
+      Image: officeImg
+    },
+    {
+      Name: "WeWork",
+      Rent: 90000,
+      Address: "Mumbai",
+      Image: officeImg
+    }
+  ];
+
+  return (
+    <div className="container">
+
+      <h1>Office Space, at Affordable Range</h1>
+
+      {
+        offices.map((item, index) => (
+
+          <div key={index} className="officeCard">
+
+            <img
+              src={item.Image}
+              width="250"
+              height="200"
+              alt="Office Space"
+            />
+
+            <h2>Name: {item.Name}</h2>
+
+            <h3
+              style={{
+                color: item.Rent <= 60000 ? "red" : "green"
+              }}
+            >
+              Rent: Rs. {item.Rent}
+            </h3>
+
+            <h3>Address: {item.Address}</h3>
+
+          </div>
+
+        ))
+      }
+
+    </div>
+  );
+}
+
+export default App;
