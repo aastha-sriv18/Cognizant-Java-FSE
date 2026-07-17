695bacc09bf5acb3e1f6e0bc66e49a63586b9632
Aastha Srivastava <aastha18p@gmail.com>
2026-07-17 20:37:10 +0530

    JUnit Exercises

---
+++ JUnit Exercises/Exercise4/src/test/java/CalculatorTest.java
@@ -0,0 +1,51 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CalculatorTest {
+
+    private Calculator calculator;
+
+    @Before
+    public void setUp() {
+        // Arrange
+        calculator = new Calculator();
+        System.out.println("Setup completed.");
+    }
+
+    @After
+    public void tearDown() {
+        calculator = null;
+        System.out.println("Teardown completed.");
+    }
+
+    @Test
+    public void testAddition() {
+
+        // Arrange
+        int a = 10;
+        int b = 20;
+
+        // Act
+        int result = calculator.add(a, b);
+
+        // Assert
+        assertEquals(30, result);
+    }
+
+    @Test
+    public void testSubtraction() {
+
+        // Arrange
+        int a = 20;
+        int b = 5;
+
+        // Act
+        int result = calculator.subtract(a, b);
+
+        // Assert
+        assertEquals(15, result);
+    }
+}
